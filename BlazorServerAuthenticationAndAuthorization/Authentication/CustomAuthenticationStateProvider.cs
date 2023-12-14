b990cc91484271545f88e8d53dc7847bab725014
mushfiq1998 <kazimushfiqcse@gmail.com>
2023-12-14 11:57:20 +0600

    Add project files.

---
+++ BlazorServerAuthenticationAndAuthorization/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+// Contains the identity and role information about the user
+using System.Security.Claims;
+
+namespace BlazorServerAuthenticationAndAuthorization.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+		// ProtectedSessionStorage for storing user session data securely in the browser.
+		private readonly ProtectedSessionStorage _sessionStorage;
+
+		// _anonymous for unautheticated user. a "claim" is a piece of information about a user or system entity.
+		// ClaimsPrincipal is used to represent an anonymous (unauthenticated) user, and designed to work with claims-based identity systems
+		// A ClaimsPrincipal can be composed of multiple ClaimsIdentity instances. 
+		private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public CustomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage)
+        { 
+            _sessionStorage = sessionStorage;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                await Task.Delay(5000);
+                var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+                var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+                if (userSession == null)
+                    return await Task.FromResult(new AuthenticationState(_anonymous));
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.UserName),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }, "CustomAuth"));
+                return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+            }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(_anonymous));
+            }
+        }
+
+        public async Task UpdateAuthenticationState(UserSession userSession)
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if (userSession != null)
+            {
+                await _sessionStorage.SetAsync("UserSession", userSession);
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.UserName),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }));
+            }
+            else
+            {
+                await _sessionStorage.DeleteAsync("UserSession");
+                claimsPrincipal = _anonymous;
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }
+}
---
+++ BlazorServerAuthenticationAndAuthorization/Authentication/UserAccountService.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorServerAuthenticationAndAuthorization.Authentication
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> _users;
+
+        public UserAccountService()
+        {
+            _users = new List<UserAccount>
+            {
+                new UserAccount{ UserName = "admin", Password = "admin", Role = "Administrator" },
+                new UserAccount{ UserName = "user", Password = "user", Role = "User" },
+            };
+        }
+
+        //UserAccount? means that the method GetByUserName can return a valid UserAccount object or null. 
+        public UserAccount? GetByUserName(string userName)
+        {
+            return _users.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+
+}
